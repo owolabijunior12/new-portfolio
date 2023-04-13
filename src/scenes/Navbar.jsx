d17dc4fca10179abf1d28391b01c23abe9b09fd5
Owolabi Destiny <107050551+owolabijunior12@users.noreply.github.com>
2023-04-13 11:38:30 +0100

    small changes

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
   const navbarBackground = isTopOfPage ? "" : "bg-red";
 
   return (
-    <nav className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
+    <nav className={`${navbarBackground} bg-black z-40 w-full fixed top-0 py-6`}>
       <div className="flex items-center justify-between mx-auto w-5/6">
         <h4 className="font-playfair text-3xl font-bold">Owolabi Destiny Oluwanifemi</h4>
       
