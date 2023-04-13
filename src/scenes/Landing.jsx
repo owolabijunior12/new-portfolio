17d2ba20b3d3c96c55b18a6ac4864e1c6249a46f
Owolabi Destiny <107050551+owolabijunior12@users.noreply.github.com>
2023-04-13 12:18:16 +0100

    small changes

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -8,7 +8,7 @@ const Landing = ({ setSelectedPage }) => {
   return (
     <section
       id="home"
-      className="md:flex md:justify-between md:items-center gap-16 md:h-full py-10"
+      className="md:flex md:justify-between md:items-center gap-16 md:h-full py-16"
     >
     
       <div className="basis-3/5 z-10 mt-16 md:mt-32 flex justify-center md:order-2">
