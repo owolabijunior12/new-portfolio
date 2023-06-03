94021c8b092df29ba07ff154acd2f5420c379e21
Owolabi Destiny <107050551+owolabijunior12@users.noreply.github.com>
2023-06-03 14:40:16 +0100

    update project

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+export {default as musicPlayerJs} from "./project-1.png";
+export {default as mernStackMA} from "./project-4.png";
+export {default as digital} from "./project-3.png";
+export {default as signUpPopUp} from "./project-2.png";
+// export {default as signUpPopUp} from "./project-2.png";
--- src/scenes/Projects.jsx
+++ src/scenes/Projects.jsx
@@ -1,3 +1,5 @@
+import { AiFillGithub, } from "react-icons/ai";
+import{BsSend } from 'react-icons/bs';
 import LineGradient from "../components/LineGradient";
 import { motion } from "framer-motion";
 
@@ -16,21 +18,21 @@ const projectVariant = {
 };
 
 const Project = ({ title }) => {
-  const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
-    bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
   const projectTitle = title.split(" ").join("-").toLowerCase();
 
   return (
-    <motion.div variants={projectVariant} className="relative">
-      <div className={overlayStyles}>
-        <p className="text-2xl font-playfair">
-
+    <motion.div variants={projectVariant} className="relative ">     
+      <div className="absolute h-full  opacity-0 hover:opacity-90 transition duration-500
+    bg-grey z-30 flex flex-col w-full l text-center p-16 text-deep-blue">
+        <p className="text-4xl   font-bold bg-black w-full h-full text-white flex justify-evenly  items-center">
+            <AiFillGithub/>
+            <BsSend/>
         </p>
         <p className="mt-7">   
         </p>
       </div>
-      <img src={`../assets/${projectTitle}.png`} alt={projectTitle} />
-      image.png
+      <img src={`../assets/${projectTitle}.png`} alt={projectTitle} />   
+
     </motion.div>
   );
 };
@@ -39,7 +41,7 @@ const Projects = () => {
   return (
     <section id="projects" className="pt-48 pb-48">
       <motion.div
-        className="md:w-2/5 mx-auto text-center"
+        className="md:w-2/5 mx-auto  gap-5 text-center"
         initial="hidden"
         whileInView="visible"
         viewport={{ once: true, amount: 0.5 }}
@@ -51,7 +53,7 @@ const Projects = () => {
       >
         <div>
           <p className="font-playfair font-semibold text-4xl">
-            <span className="text-red">PRO</span>JECTS
+            PROJECTS
           </p>
           <div className="flex justify-center mt-5">
             <LineGradient width="w-2/3" />
@@ -68,26 +70,24 @@ const Projects = () => {
           variants={container}
           initial="hidden"
           whileInView="visible"
-          viewport={{ once: true, amount: 0.2 }}
+          viewport={{ once: true, amount: 0.2 }}          
         >
-          {/* ROW 1 */}
+       
           <div
             className="flex justify-center text-center items-center p-10 bg-red
               max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
           >
             BEAUTIFUL USER INTERFACES
           </div>
-          <Project title="Project 1" />         
+          <Project title="Project 1" 
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          />         
           <Project title="Project 2" />
-
-          {/* ROW 2 */}
           <Project title="Project 3" />
           <Project title="Project 4" />
-          {/* <Project title="Project 5" /> */}
-
-          {/* ROW 3 */}
-          {/* <Project title="Project 6" /> */}
-          {/* <Project title="Project 7" /> */}
+          <Project title="Project 5" />        
+          <Project title="Project 6" />
+          <Project title="Project 7" />
           <div
             className="flex justify-center text-center items-center p-10 bg-blue
               max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
